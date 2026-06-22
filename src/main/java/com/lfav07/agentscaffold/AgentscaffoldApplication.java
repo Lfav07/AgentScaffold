9d384e2ce1e61c1b305f4ef65179072a20fabf13
lfav07 <lfavieira07@gmail.com>
2026-06-22 17:13:51 -0300

    First commit

---
+++ src/main/java/com/lfav07/agentscaffold/AgentscaffoldApplication.java
@@ -0,0 +1,13 @@
+package com.lfav07.agentscaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentscaffoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentscaffoldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lfav07/agentscaffold/AgentscaffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lfav07.agentscaffold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentscaffoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
