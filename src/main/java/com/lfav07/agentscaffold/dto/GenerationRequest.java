65fe544103977dc17f23f83fe66d5423c1489bd7
lfav07 <lfavieira07@gmail.com>
2026-06-23 17:49:06 -0300

    chore: cleanut imports

--- src/main/java/com/lfav07/agentscaffold/dto/GenerationRequest.java
+++ src/main/java/com/lfav07/agentscaffold/dto/GenerationRequest.java
@@ -1,6 +1,10 @@
 package com.lfav07.agentscaffold.dto;
 
-import com.lfav07.agentscaffold.model.*;
+import com.lfav07.agentscaffold.model.agent.AdditionalAgentType;
+import com.lfav07.agentscaffold.model.agent.CoreAgentType;
+import com.lfav07.agentscaffold.model.preset.GenerationPreset;
+import com.lfav07.agentscaffold.model.stack.BackendStack;
+import com.lfav07.agentscaffold.model.stack.FrontendStack;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
