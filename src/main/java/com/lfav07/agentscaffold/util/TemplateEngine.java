f1904dfa932a2223fb2165fb086e61cb273d731c
lfav07 <lfavieira07@gmail.com>
2026-06-26 17:32:41 -0300

    chore: move template engine class to proper directory

--- src/main/java/com/lfav07/agentscaffold/util/TemplateEngine.java
+++ src/main/java/com/lfav07/agentscaffold/util/TemplateEngine.java
@@ -1,23 +1,17 @@
-package com.lfav07.agentscaffold.service.impl;
+package com.lfav07.agentscaffold.util;
 
 import com.lfav07.agentscaffold.dto.AgentExecutionUnit;
 import com.lfav07.agentscaffold.dto.AgentRenderContext;
 import com.lfav07.agentscaffold.exception.TemplateNotFoundException;
-import com.lfav07.agentscaffold.model.agent.CoreAgentType;
-import com.lfav07.agentscaffold.model.stack.BackendStack;
-import com.lfav07.agentscaffold.resolver.ContextResolver;
 import com.samskivert.mustache.Mustache;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.stereotype.Service;
-
+import org.springframework.stereotype.Component;
 import java.io.IOException;
-import java.io.StringReader;
-import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
-@Service
-public class TemplateEngineServiceImpl {
+@Component
+public class TemplateEngine {
     private static final String TEMPLATES_PATH = "templates";
 
     public String buildAgent(
@@ -44,16 +38,5 @@ public class TemplateEngineServiceImpl {
             throw new TemplateNotFoundException(e.getMessage());
         }
         return content;
-    }
-
-    //test
-    static void main() {
-        ContextResolver contextResolver = new ContextResolver();
-        TemplateEngineServiceImpl templateEngineService = new TemplateEngineServiceImpl();
-        AgentExecutionUnit unit = new AgentExecutionUnit(CoreAgentType.BACKEND_ARCHITECT, BackendStack.JAVA_SPRING);
-      //  AgentRenderContext context = new AgentRenderContext("pizza!", "loadThis!");
-        AgentRenderContext context = contextResolver.resolve(unit, "Pizzeria");
-        String s = templateEngineService.buildAgent(unit, context);
-        System.out.println(s);
     }
 }
