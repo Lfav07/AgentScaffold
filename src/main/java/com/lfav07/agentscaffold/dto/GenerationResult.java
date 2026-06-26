42131813d510406723e410e132127d8dbd8f1103
lfav07 <lfavieira07@gmail.com>
2026-06-26 17:29:46 -0300

    feat: include filename in generation result DTO

--- src/main/java/com/lfav07/agentscaffold/dto/GenerationResult.java
+++ src/main/java/com/lfav07/agentscaffold/dto/GenerationResult.java
@@ -1,9 +1,7 @@
 package com.lfav07.agentscaffold.dto;
 
-import java.util.zip.ZipFile;
-import java.util.zip.ZipOutputStream;
 
 public record GenerationResult(
-        byte[] zip
-) {
-}
+        byte[] zip,
+        String filename
+) {}
