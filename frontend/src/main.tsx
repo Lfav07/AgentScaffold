2a169d4fe21aa2a89ae6b5dbf189c847bfd62b6b
lfav07 <lfavieira07@gmail.com>
2026-07-02 15:18:37 -0300

    feat: add tailwindcss dependency

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// @ts-ignore
 import App from './App.tsx'
+import {BrowserRouter, Route, Routes} from "react-router";
+import HomePage from "./features/home/pages/HomePage.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <BrowserRouter>
+          <Routes>
+              <Route index element={<HomePage></HomePage>}></Route>
+          </Routes>
+      </BrowserRouter>
   </StrictMode>,
 )
