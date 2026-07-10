cd9fd8a748751bac7470f3de25703430876a77a1
lfav07 <lfavieira07@gmail.com>
2026-07-09 23:15:37 -0300

    feat: add shadcn/ui dependency

--- frontend/src/features/home/pages/HomePage.tsx
+++ frontend/src/features/home/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import Header from "../../../shared/components/Header/Header";
+import {Button} from "@/components/ui/button.tsx";
 
 export default function HomePage() {
     return <>
         <Header/>
         <main className={"bg-blue-500"}>
         </main>
+        <Button>Generate</Button>
         <h1>Home page!</h1>
     </>
 }
