2d1b5957940a686aef8f67c1be429382705a5c2c
lfav07 <lfavieira07@gmail.com>
2026-07-12 17:45:44 -0300

    fix: progress bar considering one less step in display

--- frontend/src/features/generation/components/ProgressIndicator.tsx
+++ frontend/src/features/generation/components/ProgressIndicator.tsx
@@ -7,7 +7,7 @@ import type {formSteps} from "@/features/generation/types.ts";
 
 export default  function ProgressIndicator({step}: formSteps){
     return (
-        <Progress value={step / 3 * 100}>
+        <Progress value={4 / 4 * 100}>
             <ProgressLabel></ProgressLabel>
             <h1>Step {step + 1} of 4</h1>
         </Progress>
