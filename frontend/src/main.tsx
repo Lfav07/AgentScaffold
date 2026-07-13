36ad8f1bb96ded7946638aacee1fe3e1b90e99c9
lfav07 <lfavieira07@gmail.com>
2026-07-13 17:40:27 -0300

    chore: add QueryClientProvider to application

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// @ts-ignore
-import App from './App.tsx'
 import {BrowserRouter, Route, Routes} from "react-router";
 import HomePage from "./features/home/pages/HomePage.tsx";
 import GenerationPage from "@/features/generation/pages/GenerationPage.tsx";
-
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import './App.css'
+const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+      <QueryClientProvider client={queryClient}>
       <BrowserRouter>
           <Routes>
               <Route path={'/'} element={<HomePage></HomePage>} />
               <Route path={'/generate'} element={<GenerationPage />}/>
           </Routes>
       </BrowserRouter>
-  </StrictMode>,
+      </QueryClientProvider>
+  </StrictMode>
 )
