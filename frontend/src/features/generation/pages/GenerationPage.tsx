9a37bc82fa5761ceba079a38721c338b1968e377
lfav07 <lfavieira07@gmail.com>
2026-07-20 12:28:22 -0300

    fix: update gradient being displayed wrong in mobile devices

--- frontend/src/features/generation/pages/GenerationPage.tsx
+++ frontend/src/features/generation/pages/GenerationPage.tsx
@@ -3,7 +3,7 @@ import MultiStepForm from "@/features/generation/components/MultiStepForm.tsx";
 export default function GenerationPage() {
 
     return (
-        <div className="min-h-screen overflow-x-hidden bg-gradient-to-r from-white to-[#abbaab] dark:from-[#0a0a0f] dark:to-[#1a2e2e]">
+        <div className="min-h-screen bg-gradient-to-r from-white to-[#abbaab] dark:from-[#0a0a0f] dark:to-[#1a2e2e]">
             <Header/>
             <main className={"min-h-screen"}>
             <MultiStepForm/>
